bd3b8cab0aee3eb84263f3a8e0df2678a29f50ca
Vaibhavr699 <78257023+Vaibhavr699@users.noreply.github.com>
2026-04-15 23:52:48 +0530

    fixes

---
+++ Ecosysteme_ai/src/components/CooConnectWhatsapp.jsx
@@ -0,0 +1,11 @@
+import CommingSoon from "./CommingSoon";
+
+function CooConnectWhatsapp() {
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <CommingSoon />
+        </div>
+    )
+}
+
+export default CooConnectWhatsapp
