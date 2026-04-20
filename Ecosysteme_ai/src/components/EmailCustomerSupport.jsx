2827c3979dbcce43d3e4676152d9888fee9f959e
Vaibhavr699 <78257023+Vaibhavr699@users.noreply.github.com>
2026-04-20 23:26:38 +0530

    email

---
+++ Ecosysteme_ai/src/components/EmailCustomerSupport.jsx
@@ -0,0 +1,8 @@
+import CustomChat from "./CustomChat"
+import calinaImg from "../assets/svg/calina_msg_logo.svg"
+import emailImg from "../assets/svg/email_customer_support.svg";
+import { useTranslation } from "react-i18next";
+
+
+
+export default EmailCustomerSupport
