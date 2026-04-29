7165e7179a914bd0d56fb1c9e5f95b40f51af540
Vaibhavr699 <78257023+Vaibhavr699@users.noreply.github.com>
2026-04-29 21:44:38 +0530

    feat(store): add Redux store with auth, profile, navbar, agentSkills, and countryCode slices

---
+++ Ecosysteme_ai/src/store/index.jsx
@@ -0,0 +1,30 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './authSlice';
+import profileReducer from './profileSlice';
+import navbarReducer from './navbarSlice'
+import countryReducer from './countryCodeSlice'
+import agentSkillsReducer from './agentSkillsSlice'
+
+/**
+ * Configures the Redux store with application reducers.
+ *
+ * - `auth`: Manages authentication state (user, token, etc.).
+ * - `profile`: Manages user profile state (details, updates, etc.).
+ *
+ * Redux Toolkit’s `configureStore` automatically:
+ * - Sets up Redux DevTools (in development).
+ * - Adds middleware like `redux-thunk` by default.
+ *
+ * @type {import('@reduxjs/toolkit').EnhancedStore}
+ */
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    profile: profileReducer,
+    navbar: navbarReducer,
+    country: countryReducer,
+    skills: agentSkillsReducer
+  },
+});
+
+export default store;
